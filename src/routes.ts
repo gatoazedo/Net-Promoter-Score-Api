7ae1b27452b12657cf4d8ca26cdcd9b43b048576
Guilherme Azevedo <guilhermeazevedo.dev@gmail.com>
2021-02-26 16:42:36 -0300

    Survey route created.

--- src/routes.ts
+++ src/routes.ts
@@ -8,6 +8,8 @@ const userController = new UserController()
 const surveysController = new SurveysController()
 
 router.post('/user', userController.create)
+
+router.get('/surveys', surveysController.show)
 router.post('/surveys', surveysController.create)
 
 export {router}
