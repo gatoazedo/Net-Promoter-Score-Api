cb2d8ea6766b11953deff18cf54658bfeab10c2a
Guilherme Azevedo <guilhermeazevedo.dev@gmail.com>
2021-02-25 22:08:56 -0300

    TypeORM configuration.

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,12 +1,20 @@
-import {Request, Response} from 'express'
+import { Request, Response } from 'express'
 import { getRepository } from 'typeorm'
-import {User} from '../models/User'
+import { User } from '../models/User'
 
 class UserController {
 	async create(request: Request, response: Response) {
 		const {name, email} = request.body
 		
 		const usersRepository = getRepository(User)
+
+		const userAlreadyExist = await usersRepository.findOne({email})
+
+		if(userAlreadyExist) {
+			return response.status(400).json({
+				error: 'User already exists'
+			})
+		}
 
 		const user = usersRepository.create({
 			name, email
