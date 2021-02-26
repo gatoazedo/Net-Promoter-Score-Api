28651429773488c97dc42ba2e57c955f479c4c5b
Guilherme Azevedo <guilhermeazevedo.dev@gmail.com>
2021-02-25 21:57:11 -0300

    User controller created.

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,11 +1,20 @@
 import {Request, Response} from 'express'
+import { getRepository } from 'typeorm'
+import {User} from '../models/User'
 
 class UserController {
 	async create(request: Request, response: Response) {
-		const body = request.body
-		console.log(body)
+		const {name, email} = request.body
+		
+		const usersRepository = getRepository(User)
 
-		return response.send()
+		const user = usersRepository.create({
+			name, email
+		})
+
+		await usersRepository.save(user)
+
+		return response.json(user)
 	}
 }
 
