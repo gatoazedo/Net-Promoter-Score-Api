f29ef29e9f208c24cf0bafc2f017b63fe00c42e5
Guilherme Azevedo <guilhermeazevedo.dev@gmail.com>
2021-02-27 15:55:42 -0300

    User creation test finished.

--- src/__tests__/User.test.ts
+++ src/__tests__/User.test.ts
@@ -1,10 +1,21 @@
 import request from 'supertest'
 import { app } from '../app'
 
-describe('Users', () => {
-  request(app).post('/users')
-  .send({
-    email: 'user@example.com',
-    name: 'User Example'
+import createConnection from '../database'
+
+describe('Users',  () => {
+  beforeAll(async () => {
+    const connection = await createConnection()
+    await connection.runMigrations()
+  })
+
+  it('Should be able to create a new user.', async () => {
+    const response = await request(app).post('/users')
+    .send({
+      email: 'user@example.com',
+      name: 'User Example'
+    })
+
+    expect(response.status).toBe(201)
   })
 })
