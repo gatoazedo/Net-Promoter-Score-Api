03b29705045efbb645bfd5f12e925222523cad11
Guilherme Azevedo <guilhermeazevedo.dev@gmail.com>
2021-02-27 17:14:36 -0300

    Test implemented: E-mail already registered.

--- src/__tests__/User.test.ts
+++ src/__tests__/User.test.ts
@@ -3,19 +3,27 @@ import { app } from '../app'
 
 import createConnection from '../database'
 
-describe('Users',  () => {
+describe('Users', () => {
   beforeAll(async () => {
     const connection = await createConnection()
     await connection.runMigrations()
   })
 
   it('Should be able to create a new user.', async () => {
-    const response = await request(app).post('/users')
-    .send({
-      email: 'user@example.com',
+    const response = await request(app).post('/user').send({
+      email: 'user.test@example.com',
       name: 'User Example'
     })
 
     expect(response.status).toBe(201)
+  })
+
+  it('Should not be able to create a new user because the email is already registered.', async () => {
+    const response = await request(app).post('/user').send({
+      email: 'user.test@example.com',
+      name: 'User Example'
+    })
+
+    expect(response.status).toBe(400)
   })
 })
